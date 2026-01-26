4ef3fe20305b72f34653aed6bf45f5dcc9e7e8d2
Harsh5485 <159139115+Harsh5485@users.noreply.github.com>
2026-01-26 20:27:44 +0530

    Update rvdb.py

--- rvdb.py
+++ rvdb.py
@@ -58,9 +58,12 @@ class manage_db():
             return userkey["ul_mode"]
         else:
             return "gdrive"
-    
-    async def delete_user(self, user_id):
+async def delete_user(self, user_id):
         await self.user.delete_one({"_id": user_id})
 
+    # Is line ke peeche theek 4 spaces rakhein
+    async def can_download(self, user_id):
+        return True
 
+# Ye line bilkul left mein honi chahiye (Zero space)
 mydb = manage_db()
